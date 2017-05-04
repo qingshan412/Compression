0dabf3ae42359726da535edd38ed33f645fcc5da
qingshan412 <qingshan412@hotmail.com>
2017-05-04 15:32:16 -0400

    multi test

--- MoreS/cifar10_multi_gpu_train.py
+++ MoreS/cifar10_multi_gpu_train.py
@@ -49,7 +49,7 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000,
+tf.app.flags.DEFINE_integer('max_steps', 1000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_integer('num_gpus', 2,
                             """How many GPUs to use.""")
