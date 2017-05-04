612d300e89dc0b5ab5aaee7b11ed05d998f35c8d
qingshan412 <qingshan412@hotmail.com>
2017-05-04 12:51:33 -0400

    multi test

--- MoreS/cifar10_multi_gpu_train.py
+++ MoreS/cifar10_multi_gpu_train.py
@@ -202,7 +202,7 @@ def train():
     for grad, var in grads:
       if grad is not None:
         summaries.append(
-            tf.histogram_summary(var.op.name + '/gradients', grad))
+            tf.summary.histogram(var.op.name + '/gradients', grad))
 
     # Apply the gradients to adjust the shared variables.
     apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
